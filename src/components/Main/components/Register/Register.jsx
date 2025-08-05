a8a84fe7e116b263b6732bc9d26ea7db73451912
VicksCR <vicky.castro.reyes.76@gmail.com>
2025-08-05 14:00:11 -0600

    Escribir codigo de Register jsx y css

--- src/components/Main/components/Register/Register.jsx
+++ src/components/Main/components/Register/Register.jsx
@@ -1,3 +1,7 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../../../../blocks/register.css";
+
 export default function Register({ onRegister }) {
   const [formData, setFormData] = useState({ email: "", password: "" });
 
@@ -11,24 +15,49 @@ export default function Register({ onRegister }) {
 
   return (
     //header
-    <form className="login__form" onSubmit={handleSubmit}>
-      <h2>Inicia sesión</h2>
-      <input
-        className="login__input"
-        name="email"
-        type="email"
-        onChange={handleChange}
-        value={formData.email}
-      />
-      <input
-        className="login__input"
-        name="password"
-        type="password"
-        onChange={handleChange}
-        value={formData.password}
-      />
-      <button className="login__button">Inicia sesión</button>
-    </form>
+    <div className="register">
+      <form
+        className="register__form"
+        name="register-form"
+        noValidate
+        onSubmit={handleSubmit}
+      >
+        <h2 className="register__title">Regístrate</h2>
+        <input
+          className="register__input"
+          type="email"
+          name="email"
+          placeholder="Correo electrónico"
+          onChange={handleChange}
+          value={formData.email}
+          required
+        />
+        <input
+          className="register__input"
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          onChange={handleChange}
+          value={formData.password}
+          required
+        />
+        <button
+          type="submit"
+          className="register__submit-button"
+          disabled={!formData.email || !formData.password}
+        >
+          Regístrate
+        </button>
+      </form>
+      <div className="register__redirect-container">
+        <p className="register__redirect">
+          ¿Ya eres miembro?{" "}
+          <Link to="/signin" className="register__link">
+            Inicia sesión aquí
+          </Link>
+        </p>
+      </div>
+    </div>
   );
 }
 //ACTUALIZAR DE ACUERDO A FIGMA
