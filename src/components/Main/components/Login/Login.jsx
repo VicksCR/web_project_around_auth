676677242584825ee00d75af9179a09884799439
VicksCR <vicky.castro.reyes.76@gmail.com>
2025-08-05 12:11:20 -0600

    Escibir codigo de Login jsx y css

--- src/components/Main/components/Login/Login.jsx
+++ src/components/Main/components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "../../../../blocks/login.css";
 
 export default function Login({ onLogin }) {
   const [formData, setFormData] = useState({ email: "", password: "" });
@@ -13,50 +14,53 @@ export default function Login({ onLogin }) {
   };
 
   return (
-    //header
-    <div className="login">
-      <form
-        className="login__form"
-        name="login-form"
-        noValidate
-        onSubmit={handleSubmit}
-      >
-        <h2 className="login__title">Inicia sesión</h2>
-        <label className="login__label">Correo electrónico</label>
-        <input
-          className="login__input"
-          name="email"
-          type="email"
-          placeholder="Correo electrónico"
-          onChange={handleChange}
-          value={formData.email}
-          required
-        />
-        <label className="login__label">Contraseña</label>
-        <input
-          className="login__input"
-          name="password"
-          type="password"
-          placeholder="Contraseña"
-          onChange={handleChange}
-          value={formData.password}
-          required
-        />
-        <button
-          type="submit"
-          className="login__submit-button"
-          disabled={!formData.email || !formData.password}
+    <>
+      <header></header>
+      <div className="login">
+        <form
+          className="login__form"
+          name="login-form"
+          noValidate
+          onSubmit={handleSubmit}
         >
-          Inicia sesión
-        </button>
-      </form>
-      <p className="login__redirect">
-        ¿Aún no eres miembro?
-        <Link to="/signup" style={{ color: "lightblue" }}>
-          Regístrate aquí
-        </Link>
-      </p>
-    </div>
+          <h2 className="login__title">Inicia sesión</h2>
+          <input
+            className="login__input"
+            name="email"
+            type="email"
+            placeholder="Correo electrónico"
+            onChange={handleChange}
+            value={formData.email}
+            required
+          />
+          <input
+            className="login__input"
+            name="password"
+            type="password"
+            placeholder="Contraseña"
+            onChange={handleChange}
+            value={formData.password}
+            required
+          />
+          <button
+            type="submit"
+            className="login__submit-button"
+            disabled={!formData.email || !formData.password}
+          >
+            Inicia sesión
+          </button>
+        </form>
+        <div className="login__redirect-container">
+          <p className="login__redirect">
+            ¿Aún no eres miembro?{" "}
+            <Link to="/signup" className="login__link">
+              Regístrate aquí
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
 //ACTUALIZAR DE ACUERDO A FIGMA
+//FALTA EL REGISTRATE EN <HEADER>
